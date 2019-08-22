bf2f5ad075753b1065a46d38cadfbbfcaa6b8491
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-08-22 17:49:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,15 @@ function calculatesFarePrice (start, end) {
   
   if (distance < 400) {
     return 0
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far';
   }
+  //returns the number of blocks given a value
 }
-  
+
   
  
