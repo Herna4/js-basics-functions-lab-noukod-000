14172722c1d16c1462eceb01f37e339cbc3ad59a
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-08-22 17:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function distanceTravelledInFeet (start, end) {
 function calculatesFarePrice (start, end) {
   const distance = distanceTravelledInFeet(start, end);
   
-  if (distance)
+  if (distance < 400)
+    return 0
   
 }
